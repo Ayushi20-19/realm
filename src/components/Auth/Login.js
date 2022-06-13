ffb43e41001f7b0652cfbd6d48b663f327d207e2
Ayushi Verma <50084909+Ayushi20-19@users.noreply.github.com>
2022-06-13 16:01:59 +0530

    typo

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -55,7 +55,7 @@ const Login = () => {
               value={userDetail.email}
               onChange={setUserDetailHandler}
               class='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white'
-              placeholder='name@company.com'
+              placeholder='name@gmail.com'
               required=''
             />
           </div>
